185d5bcfb2ccbc12318847316d609aec0194be30
Simon Oakwood <dabrowa.szymon@gmail.com>
2023-01-10 20:03:49 +0100

    refactoring DataTable structure

--- src/Components/DataTable.tsx
+++ src/Components/DataTable.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid/index.js";
 import { useFetchProductsQuery } from "../store";
+import { productsApi } from "../store/apis/productsApi";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 export default function DataTable() {
   const { data, error, isLoading } = useFetchProductsQuery();
+  function createData(id: number, name: string, year: number, color: string) {
+    return { id, name, year, color };
+  }
 
   let content;
   if (isLoading) {
@@ -11,10 +22,23 @@ export default function DataTable() {
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
-    let products = data?.data;
-    let rows = products;
+    let rows = data?.data.map(
+      ({
+        id,
+        name,
+        year,
+        color,
+      }: {
+        id: number;
+        name: string;
+        year: number;
+        color: string;
+      }) => createData(id, name, year, color)
+    );
+    // let products = data?.data;
+    // let rows = products;
     const columns: GridColDef[] = [
-      { field: "id", headerName: "ID", width: 70 },
+      { field: "id", headerName: "ID", width: 7 },
       { field: "name", headerName: "Name", width: 130 },
       {
         field: "year",
@@ -24,14 +48,39 @@ export default function DataTable() {
       },
     ];
     content = (
-      <div style={{ height: 400, width: "100%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-        />
-      </div>
+      // <div style={{ height: 400, width: "100%" }}>
+      //   <DataGrid
+      //     rows={rows}
+      //     columns={columns}
+      //     pageSize={5}
+      //     rowsPerPageOptions={[5]}
+      //   />
+      // </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Year</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row: any) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.year}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     );
   }
 
