9f7bf1bf981ae180fde0eae9a66298910a027870
Simon Oakwood <dabrowa.szymon@gmail.com>
2023-01-10 11:39:26 +0100

    setting up the project, adding DataTable & FormControl components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import DataTable from "./Components/DataTable";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="md">
+        <Box sx={{ p: 2, border: "1px dashed grey", margin: 5 }}>
+          <Stack
+            spacing={2}
+            sx={{
+              bgcolor: "",
+            }}
+          >
+            <FormControl>
+              <InputLabel htmlFor="component-outlined">Search by ID</InputLabel>
+              <OutlinedInput
+                id="component-outlined"
+                defaultValue="please, type just numbers"
+                label="Search by ID"
+              />
+            </FormControl>
+            <DataTable />
+          </Stack>
+        </Box>
+      </Container>
     </div>
   );
 }
