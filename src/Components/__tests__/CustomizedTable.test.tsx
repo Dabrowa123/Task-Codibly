ae1828d5093617f9c155684d07c217c7956fa868
Szymon Dabrowa <dabrowa.szymon@gmail.com>
2023-01-30 16:19:07 +0100

    refactoring tests

--- src/Components/__tests__/CustomizedTable.test.tsx
+++ src/Components/__tests__/CustomizedTable.test.tsx
@@ -3,16 +3,6 @@ import { fireEvent, screen } from "@testing-library/react";
 import { renderWithProviders } from "../../utils/test-utils";
 
 describe("CustomizedTable Component", () => {
-  it("should change pagination label to 6 - 10 of 12 after first click on pagination button", () => {
-    renderWithProviders(<CustomizedTable />);
-    const paginationLabel = screen.getByTestId("pagination");
-    const nextPageButton = screen.getByTitle(
-      "Go to next page"
-    ) as unknown as Element;
-    fireEvent.click(nextPageButton);
-    expect(paginationLabel).toHaveTextContent("6-10 of 12");
-  });
-
   it("should open modal after clicking a row", async () => {
     renderWithProviders(<CustomizedTable />);
     expect(await screen.findByText("1")).toBeInTheDocument();
