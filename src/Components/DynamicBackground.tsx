babbe7f7fc0eaf13ac9a7edd40426a04b9a53b64
Szymon Dabrowa <dabrowa.szymon@gmail.com>
2023-01-30 16:08:57 +0100

    refactoring dynamic background

--- src/Components/DynamicBackground.tsx
+++ src/Components/DynamicBackground.tsx
@@ -18,17 +18,12 @@ function DynamicBacground() {
   });
 
   React.useEffect(() => {
-    if (colors.length > 2) {
+    if (colors.length === 5) {
       setStrippedBackground({
         pattern: `linear-gradient(45deg, ${colors[0]} 10%, ${colors[1]} 10%, ${colors[1]} 20%, ${colors[2]} 20%, ${colors[2]} 30%, ${colors[3]} 30%, ${colors[3]} 40%, ${colors[4]} 40%, ${colors[4]} 50%, ${colors[0]} 50%, ${colors[0]} 60%, ${colors[1]} 60%, ${colors[1]} 70%, ${colors[2]} 70%, ${colors[2]} 80%, ${colors[3]} 80%, ${colors[3]} 90%, ${colors[4]} 90%, ${colors[4]} 100%)`,
         size: "1200px 1200px",
       });
-    } else if (colors.length === 2) {
-      setStrippedBackground({
-        pattern: `linear-gradient(45deg, ${colors[1]} 25%, ${colors[0]} 25%, ${colors[0]} 50%, ${colors[1]} 50%, ${colors[1]} 75%, ${colors[0]} 75%, ${colors[0]} 100%)`,
-        size: "600px 600px",
-      });
-    } else if (colors.length < 2) {
+    } else {
       setStrippedBackground({
         pattern: `linear-gradient(45deg, #ffffff 25%, ${colors[0]} 25%, ${colors[0]} 50%, #ffffff 50%, #ffffff 75%, ${colors[0]} 75%, ${colors[0]} 100%)`,
         size: "600px 600px",
